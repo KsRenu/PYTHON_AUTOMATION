bbc893b79e422f5017bdb89ac818a4afe72cd9b6
KsRenu <renuks0802@gmail.com>
2023-04-12 05:38:53 +0530

    updating the code  to updates in package

--- 1.Simple Google search automation/1.Simple Google search automatin.py
+++ 1.Simple Google search automation/1.Simple Google search automatin.py
@@ -1,12 +1,15 @@
-#import selenium
+
 from selenium import webdriver 
+from selenium.webdriver.common.by import By
+
 import time
 find=input("Enter what you need to search : ")
-browser=webdriver.Chrome(executable_path="C:\\Users\\Admin\\Downloads\\chromedriver_win32\\chromedriver") 
+browser=webdriver.Chrome(executable_path="F:\\GitHub\\pythonAutomation\\chromedriver_win32\\chromedriver") 
 browser.get('https://www.google.com/')
-time.sleep(3)
-search_input=browser.find_element_by_name('q')
+time.sleep(3)#search_input=browser.find_element_by_name('q')
+search_input=browser.find_element("name", "q")
 search_input.send_keys(find)
 time.sleep(2)
-search_btn=browser.find_element_by_css_selector('input[type="submit"]')
+#search_btn=browser.find_element_by_css_selector('input[type="submit"]')
+search_btn=browser.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
 search_btn.click()
